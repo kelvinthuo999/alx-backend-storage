25c0e5c71fe1d0d1a65e222de29e096eabc2e2b4
kelvinthuo999 <thoukelvinngigi@gmail.com>
2024-04-25 15:53:59 +0000

    create: script to compute and store avg score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,35 +1,12 @@
 --script that calculates and stores average score
-DELIMITER //
-
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
+    IN user_id INT)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE total_projects INT;
-    DECLARE average_score FLOAT;
-
-    -- Compute total score for the user
-    SELECT SUM(score) INTO total_score
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Compute total number of projects for the user
-    SELECT COUNT(DISTINCT project_id) INTO total_projects
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Compute average score
-    IF total_projects > 0 THEN
-        SET average_score = total_score / total_projects;
-    ELSE
-        SET average_score = 0;
-    END IF;
-
-    -- Update user's average score
-    UPDATE users
-    SET average_score = average_score
-    WHERE id = user_id;
-END //
-
+    DECLARE avg_score FLOAT;
+    SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
+END
+$$
 DELIMITER ;

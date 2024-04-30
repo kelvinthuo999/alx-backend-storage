33d5f71a9f087ed17681ed93d00c98cd5b986fea
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-04-30 02:21:35 -0700

    create: python script to insert

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -6,5 +6,4 @@ import pymongo
 
 def insert_school(mongo_collection, **kwargs):
     '''Function to insert into a doc'''
-    result = mongo_collection.insert(kwargs)
-    return result.inserted_id
+    return mongo_collection.insert(kwargs)

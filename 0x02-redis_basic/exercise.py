1a578f99daeb829759bb82d6dfd7bc052b979afc
kelvinthuo999 <thuokelvinngigi@gmail.com>
2024-05-01 04:26:14 -0700

    create: script to count the num of time Cache is called

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,8 @@
 
 import redis
 import uuid
-from typing import Union, Callable
+from typing import Union, Callable, Any
+import functools
 
 
 class Cache:
@@ -12,7 +13,20 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @staticmethod
+    def count_calls(method: Callable) -> Callable:
+        '''count calls decorator'''
+        @functools.wraps(method)
+        def wrapper(self, *args, **kwargs) -> Any:
+            '''wrapper function'''
+            key = f"calls:{method.__qualname__}"
+            self._redis.incr(key)
+            return method(self, *args, **kwargs)
+        return wrapper
+
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''store data in redis'''
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
@@ -20,6 +34,7 @@ class Cache:
     def get(
             self, key: str, fn: Callable = None
             ) -> Union[str, bytes, int, float, None]:
+        '''get data from redis'''
         value = self._redis.get(key)
         if value is None:
             return None
@@ -28,7 +43,9 @@ class Cache:
         return value
 
     def get_str(self, key: str) -> Union[str, None]:
+        '''get string data from redis'''
         return self.get(key, fn=lambda d: d.decode("utf-8"))
 
     def get_int(self, key: str) -> Union[int, None]:
+        '''get integer data from redis'''
         return self.get(key, fn=lambda d: int(d))
